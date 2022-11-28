5ca33f59571016ad79baea5b84fd2538a07ff6c6
WillDevAc <wydemairomsilva@gmail.com>
2022-11-28 18:13:40 -0300

    review: code review

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,18 +1,22 @@
-import React from 'react';
+import React from "react";
 
-import './styles.scss'
+import "./styles.scss";
 
 const Auth: React.FC = () => {
   return (
     <div className="container">
-        <div className="wrapper_avatar">
-            <img src="#" alt="avatar" />
+      <div className="wrapper_avatar">
+        <img src="/public/icon.svg" alt="avatar" />
+        <p>VOLTAR PARA HOME</p>
+      </div>
+      <div className="wrapper">
+        <div className="form">form</div>
+        <div className="avatar">
+          <img src="#" alt="avatar" />
         </div>
-        <div className="wrapper">
-            <span>Wrapper</span>
-        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default Auth;
